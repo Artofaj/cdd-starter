20007d824df913d09b3f5ee97834836c024ff54c
Artofaj <info@artofakume.com>
2024-01-02 11:44:21 +0100

    Add Sanity plugins and config

--- CMS/sanity.config.ts
+++ CMS/sanity.config.ts
@@ -1,16 +1,37 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+import {
+  dashboardTool,
+  projectInfoWidget,
+  projectUsersWidget,
+} from "@sanity/dashboard";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 export default defineConfig({
   name: 'default',
   title: 'CMS',
-
   projectId: '8tszoh22',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [dashboardTool({
+    widgets: [
+      projectInfoWidget(),
+      projectUsersWidget(),
+      netlifyWidget({
+        sites: [
+          {
+            title: "Art of Akume",
+            apiId: import.meta.env.SANITY_STUDIO_NETLIFY_PROJECT_ID,
+            buildHookId: import.meta.env.SANITY_STUDIO_NETLIFY_HOOK_ID,
+            name: "artofakume",
+            url: "https://www.artofakume.com",
+          },
+        ],
+      }),
+    ],
+  }), deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
