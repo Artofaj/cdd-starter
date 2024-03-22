00839b61f72a47ec947b3dea8615e13115f6920b
Artofaj <info@artofakume.com>
2024-03-22 11:25:54 +0100

    feat: implement Header

--- FE/src/components/layout.tsx
+++ FE/src/components/layout.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createOurTheme } from '../assets/themes/theme';
 import '../assets/themes/fonts.css';
+import Header from './Header';
 
 const Layout = ({ children }) => {
     return (
         <ThemeProvider theme={createOurTheme()}>
             <CssBaseline />
+            <Header title="CDD Starter" />
             <div>
                 <main>{children}</main>
                 <footer></footer>
